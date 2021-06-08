dfd833ff6f635e01f47d36b54ea01b5f67e1e017
GreenXenith <24834740+GreenXenith@users.noreply.github.com>
2021-06-08 13:32:59 -0700

    Add help command

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const levels = require("./levels.js");
-const {prefix, token} = require("./config.json");
+const config = require("./config.json");
 
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}.`);
@@ -45,21 +45,52 @@ const commands = new (function() {
         message.channel.send(levels.getTop());
     }
     this.top = this.levels;
+
+    this.help = (message) => {
+        let c;
+        message.channel.send({
+            "embed": {
+                "color": config.color,
+                "thumbnail": {
+                    "url": client.user.avatarURL(),
+                },
+                "title": "MeseBot Info",
+                "description": `Minetest-themed levelling bot.\nPrefix: \`${config.prefix}\``,
+                "fields": [
+                    {
+                        "name": "Commands",
+                        "value": Object.entries(commands).map(e => {const s = `${c && e[1] == c ? `, ` : "\n"}\`${e[0]}\``; c = e[1]; return s;}).join("").slice(1),
+                        "inline": true
+                    },
+                    {
+                        "name": "Rank Rewards",
+                        "value": (config.role_rewards || []).sort((a, b) => a[0] - b[0]).map(e => `${e[0]}: <@&${e[1]}>`).join("\n"),
+                        "inline": true
+                    },
+                    {
+                        "name": "Source",
+                        "value": "https://github.com/MinetestBots/mesebot"
+                    }
+                ]
+            }
+        });
+    }
+    this.info = this.help;
 });
 
 client.on("message", message => {
     if (message.author.bot)	return;
 
     // Do user updating
-    if (!message.content.startsWith(prefix)) {
+    if (!message.content.startsWith(config.prefix)) {
         levels.newMessage(message);
         return;
     }
 
     // Check for command
-    const params = message.content.slice(prefix.length).split(" ");
+    const params = message.content.slice(config.prefix.length).split(" ");
     const command = params.shift().toLowerCase();
     if (commands[command]) commands[command](message, params);
 });
 
-client.login(token);
+client.login(config.token);
