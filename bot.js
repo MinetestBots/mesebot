77143363b960d760f3a34ee50bada9a13b2c1515
Panquesito7 <halfpacho@gmail.com>
2021-07-30 18:39:46 +0000

    feat: Make the bot have a typing delay...
    ...that looks as if the bot is writing, like a normal user typing in a channel.

--- bot.js
+++ bot.js
@@ -5,6 +5,21 @@ const roles = require ("./roles.js");
 const levels = require("./levels.js");
 const config = require("./config.json");
 
+// Sleep function taken from StackOverflow
+// https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+Discord.TextChannel.prototype.typeMessage = 
+  async function (message) {
+    this.startTyping();
+    await sleep(3000);
+    this.stopTyping();
+
+    this.send(message);
+}
+
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}.`);
 });
@@ -13,16 +28,16 @@ client.once("ready", () => {
 const commands = new (function() {
     this.rank = (message, params) => {
         if (!params[0]) { // Self info
-            message.channel.send(levels.getInfo(message.author))
+            message.channel.typeMessage(levels.getInfo(message.author))
         } else if (message.mentions.users.size > 0) { // Mentioned user
-            message.channel.send(levels.getInfo(message.mentions.users.first()));
+            message.channel.typeMessage(levels.getInfo(message.mentions.users.first()));
         } else { // Named user
             const members = message.channel.guild.members;
 
             // Find by nick or username
             members.fetch({query: params[0], limit: 1}).then(member => {
                 if (member.first()) {
-                    message.channel.send(levels.getInfo(member.first().user));
+                    message.channel.typeMessage(levels.getInfo(member.first().user));
                     return;
                 }
 
@@ -30,26 +45,26 @@ const commands = new (function() {
                 if (params[0].match(/\d{18}/)) {
                     if (levels.getUser(params[0]).rank > 0) {
                         client.users.fetch(params[0]).then(user => {
-                            message.channel.send(levels.getInfo(user));
+                            message.channel.typeMessage(levels.getInfo(user));
                         });
                         return;
                     }
                 }
 
-                message.channel.send(`Could not find user or id \"${params[0]}\".`);
+                message.channel.typeMessage(`Could not find user or ID \"${params[0]}\".`);
             });
         }
     }
     this.level = this.rank;
 
     this.levels = (message) => {
-        message.channel.send(levels.getTop());
+        message.channel.typeMessage(levels.getTop());
     }
     this.top = this.levels;
 
     this.help = (message) => {
         let c;
-        message.channel.send({
+        message.channel.typeMessage({
             "embed": {
                 "color": config.color,
                 "thumbnail": {
@@ -69,7 +84,7 @@ const commands = new (function() {
                         "inline": true
                     },
                     {
-                        "name": "Source",
+                        "name": "Source code",
                         "value": "https://github.com/MinetestBots/mesebot"
                     }
                 ]
